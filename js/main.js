442d54f0800159b7ef08f8674b4257c4f7c8a51a
Problemon <danilets.pasha@mail.ru>
2021-09-27 08:34:38 +1000

    Добавлены функции получения рандомного число из диапазона

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const getRandomNumberInRange = (from, to) => {
+  if (from >= to) {
+    // eslint-disable-next-line no-alert
+    alert('Неправильные диапозон');
+    return;
+  }
+
+  const rand = from + Math.random() * (to - from + 1);
+  return Number(Math.floor(rand));
+};
+// eslint-disable-next-line no-console
+console.log(getRandomNumberInRange(1, 3));
+
+
+const getRandomNumberInRangeFloat = (from, to, floatingPoint = 0) => {
+  if (from >= to) {
+    // eslint-disable-next-line no-alert
+    alert('Неправильные диапозон');
+    return;
+  }
+
+  const rand = from + Math.random() * (to - from);
+  return Number(rand.toFixed(floatingPoint));
+};
+// eslint-disable-next-line no-console
+console.log(getRandomNumberInRangeFloat(1.1, 1.2, 1));
+
+// Формула взята из https://learn.javascript.ru/task/random-int-min-max
