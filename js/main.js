45e5284132802ae2886d1f5a6fb8bd35bc673e85
Problemon <danilets.pasha@mail.ru>
2021-10-04 11:35:40 +1000

    Альфа выполнение четвертого модуля, создание объекта объявления.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,94 @@
-const getRandomNumberInRange = (from, to) => {
-  if (from >= to || from < 0) {
-    throw new Error('Неправильные диапазон');
+// Функция взята из интернета и доработана
+// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
+const getRandomNumberInRange = (first, second) => {
+  const lower = Math.ceil(Math.min(Math.abs(first), Math.abs(second)));
+  const upper = Math.floor(Math.max(Math.abs(first), Math.abs(second)));
+
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+};
+
+const getRandomNumberInRangeFloat = (first, second, digits = 0) => {
+  const lower = Math.min(Math.abs(first), Math.abs(second));
+  const upper = Math.max(Math.abs(first), Math.abs(second));
+
+  const result = Math.random() * (upper - lower) + lower;
+
+  return result.toFixed(digits);
+};
+
+
+const getRandomElementOfArray = (array) => array[getRandomNumberInRange(0, array.length - 1)];
+
+const getRandomArrayOfArray = (array) => {
+  const randomIndex = getRandomNumberInRange(1, array.length);
+  return array.slice(0, randomIndex);
+};
+
+
+const getArrayWithNumbers = (amount) => {
+  const arrayNumbers = [];
+  for (let index = 0; index < amount; index += 1) {
+    let number = index + 1;
+    if (number < 10) {
+      number = `0${number}`; // Прибавляем 0 к цивре пока чило однозначное '1 = 01'
+    }
+
+    arrayNumbers[index] = String(number);
   }
 
-  const rand = from + Math.random() * (to - from + 1);
-  return Number(Math.floor(rand));
+  return arrayNumbers;
+};
+
+const numbersOfAvatar = getArrayWithNumbers(10);
+
+const createAnnouncement = () => {
+  const announce = {
+    createAvatar() {
+      const randomIndex = getRandomNumberInRange(0, numbersOfAvatar.length - 1);
+      const numberOfAvatar = numbersOfAvatar.splice(randomIndex, 1);
+      this.author.avatar = `img/avatars/user${numberOfAvatar}.png`;
+    },
+    createAddress() {
+      const lat = this.location.lat;
+      const lng = this.location.lng;
+      this.offer.address = `${lat}, ${lng}`;
+    },
+    author: {
+      avatar: `img/avatars/user${this}.png`,
+    },
+    offer: {
+      title: 'Sell annoucement',
+      features: getRandomArrayOfArray(['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner']),
+      checkin:  getRandomElementOfArray(['12:00', '13:00', '14:00']),
+      checkout: getRandomElementOfArray(['12:00', '13:00', '14:00']),
+      type:     getRandomElementOfArray(['palace', 'flat', 'house', 'bungalow', 'hotel']),
+      price:    getRandomNumberInRange(0, 10),
+      rooms:    getRandomNumberInRange(0, 10),
+      quests:   getRandomNumberInRange(0, 10),
+      description: 'Best place',
+      photos:   getRandomArrayOfArray([
+        'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+        'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+        'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+      ]),
+    },
+    location: {
+      lat: getRandomNumberInRangeFloat(35.65000, 35.70000, 5),
+      lng: getRandomNumberInRangeFloat(139.70000, 139.80000, 5),
+    },
+  };
+  announce.createAvatar();
+  announce.createAddress();
+
+  return announce;
+};
+
+const getArrayObject = (number) => {
+  const arrayAnnouncements = Array.from({length: number}, createAnnouncement);
+
+  return arrayAnnouncements;
 };
 // eslint-disable-next-line no-console
-console.log(getRandomNumberInRange(-2, 3));
-
-
-const getRandomNumberInRangeFloat = (from, to, floatingPoint = 0) => {
-  if (from >= to || from < 0) {
-    throw new Error('Неправильные диапазон');
-  }
-
-  const rand = from + Math.random() * (to - from);
-  return Number(rand.toFixed(floatingPoint));
-};
-// eslint-disable-next-line no-console
-console.log(getRandomNumberInRangeFloat(1.1, 1.2, 1));
-
-// Формула взята из https://learn.javascript.ru/task/random-int-min-max
+console.log(getArrayObject(10));
